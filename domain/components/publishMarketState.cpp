dba0c98b04f3e0bb29cca7d05099a1a4c4a67d7b
zhoufan <zhoufan@cdsslh.com>
2022-03-04 10:24:43 +0800

    [问题/需求描述]:增加日期输出 [解决方法]:增加日期输出 [影响范围]:无 [需求编号]:无
    Change-Id: Id811915aaacebc36d827028cd007eba000338713

--- domain/components/publishMarketState.cpp
+++ domain/components/publishMarketState.cpp
@@ -14,6 +14,8 @@ publishState::publishState()
 
 void publishState::publish_event(void)
 {
+    char date_buff[10];
+    get_trade_data(date_buff);
     auto& marketSer = MarketService::getInstance();
 
     auto keyNameList = marketSer.ROLE(controlPara).getKeyNameList();
@@ -41,6 +43,7 @@ void publishState::publish_event(void)
         }
 
         market_state->set_market_state(state);
+        market_state->set_date(date_buff);
         std::string tickStr;
         tick.SerializeToString(&tickStr);
         auto& recerSender = RecerSender::getInstance();
@@ -79,6 +82,9 @@ void publishState::pushlish_cycle(void)
 
 void publishState::publish_day_closing(void)
 {
+    char date_buff[10];
+    get_trade_data(date_buff);
+
     auto& marketSer = MarketService::getInstance();
 
     auto keyNameList = marketSer.ROLE(controlPara).getKeyNameList();
@@ -89,6 +95,8 @@ void publishState::publish_day_closing(void)
 
         market_strategy::TickMarketState_MarketState state = market_strategy::TickMarketState_MarketState_day_closing;
         market_state->set_market_state(state);
+        market_state->set_date(date_buff);
+
         std::string tickStr;
         tick.SerializeToString(&tickStr);
         auto& recerSender = RecerSender::getInstance();
@@ -97,4 +105,113 @@ void publishState::publish_day_closing(void)
         sleep(1);
     }
     INFO_LOG("Publish makret state.");
+}
+
+int publishState::is_leap_year(int y)
+{
+    if ((y%4 == 0 && y%100 != 0) || y%400 == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+void publishState::get_trade_data(char * buff)
+{
+    int y,m,d,a[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+
+    time_t now_time = time(NULL);
+    //local time
+    tm* local_time = localtime(&now_time);
+
+    y = 1900+local_time->tm_year;
+    m = 1 + local_time->tm_mon;
+    d = local_time->tm_mday;
+
+    if (is_leap_year(y)==1)
+    {
+        a[1] = 29;
+    }
+
+    if (20 <= local_time->tm_hour && local_time->tm_hour <= 22)
+    {
+        if (local_time->tm_wday == 5)
+        {
+            d += 3;
+            while(d>a[m-1])
+            {
+                d -= a[m-1];
+                m++;
+                if(m>12)
+                {
+                    m=1;
+                    y++;
+                    if (is_leap_year(y)==1)
+                    {
+                        a[1] = 29;
+                    }
+                    else
+                    {
+                        a[1] = 28;
+                    }
+                }
+            }
+            sprintf(buff, "%04d%02d%02d", y, m, d);
+        }
+        else
+        {
+            d += 1;
+            while(d>a[m-1])
+            {
+                d -= a[m-1];
+                m++;
+                if(m>12)
+                {
+                    m=1;
+                    y++;
+                    if (is_leap_year(y)==1)
+                    {
+                        a[1] = 29;
+                    }
+                    else
+                    {
+                        a[1] = 28;
+                    }
+                }
+            }
+            sprintf(buff, "%04d%02d%02d", y, m, d);
+        }
+    }
+    else if (1 <= local_time->tm_hour && local_time->tm_hour <= 3 && local_time->tm_wday == 6)
+    {
+        d += 2;
+        while(d>a[m-1])
+        {
+            d -= a[m-1];
+            m++;
+            if(m>12)
+            {
+                m=1;
+                y++;
+                if (is_leap_year(y)==1)
+                {
+                    a[1] = 29;
+                }
+                else
+                {
+                    a[1] = 28;
+                }
+            }
+        }
+        sprintf(buff, "%04d%02d%02d", y, m, d);
+    }
+    else
+    {
+        sprintf(buff, "%04d%02d%02d", y, m, d);
+    }
+
+    INFO_LOG("trade date: %s", buff);
 }
--- domain/components/publishMarketState.h
+++ domain/components/publishMarketState.h
@@ -6,9 +6,11 @@ public:
     publishState();
     void publish_event(void);
     void pushlish_cycle(void);
+    void get_trade_data(char *);
     ~publishState() {};
 private:
     void publish_day_closing(void);
+    int is_leap_year(int y);
 };
 
 #endif
